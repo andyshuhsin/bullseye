3a33197f4229ffc624d80ae2271326144fe9d7d9
Andy Shu Xin <andy@shux.in>
2016-10-18 14:20:13 +0800

    Enable scene changing

--- src/main.js
+++ src/main.js
@@ -1,77 +1,97 @@
 localStorage.history = localStorage.history || "[]";
 
+const uiState = {
+    scene: 'bar'
+};
 const history = JSON.parse(localStorage.history);
 
-const data = [
-    {
-        value: 1,
-        tag: "1",
-    },
-    {
-        value: 1 + Math.random() * 2,
-        tag: "?"
-    },
-];
+function renderBarScene() {
+    const data = [
+        {
+            value: 1,
+            tag: "1",
+        },
+        {
+            value: 1 + Math.random() * 2,
+            tag: "?"
+        },
+    ];
 
-const barSpacing = 20;
-const barNumber = data.length;
+    const barSpacing = 20;
+    const barNumber = data.length;
 
-const graphDom = document.querySelector(".graph");
-const graphDomWidth = graphDom.scrollWidth;
-const barWidth = graphDomWidth / barNumber - barSpacing;
+    const graphDom = document.querySelector(".graph");
+    const graphDomWidth = graphDom.scrollWidth;
+    const barWidth = graphDomWidth / barNumber - barSpacing;
 
-const graphHeight = 300;
+    const graphHeight = 300;
 
-const barChart = d3.select(".graph")
-        .append("svg:svg")
-        .attr("width", graphDomWidth)
-        .attr("height", graphHeight);
+    const barChart = d3.select(".graph")
+            .append("svg:svg")
+            .attr("width", graphDomWidth)
+            .attr("height", graphHeight);
 
-barChart.selectAll("rect")
-        .data(data).enter()
-        .append("svg:rect")
-        .attr("x", (_, index) => index * (barWidth + barSpacing))
-        .attr("y", datum => graphHeight - datum.value * 100)
-        .attr("height", datum => datum.value * 100)
-        .attr("width", barWidth)
-        .attr("fill", "#888");
+    barChart.selectAll("rect")
+            .data(data).enter()
+            .append("svg:rect")
+            .attr("x", (_, index) => index * (barWidth + barSpacing))
+            .attr("y", datum => graphHeight - datum.value * 100)
+            .attr("height", datum => datum.value * 100)
+            .attr("width", barWidth)
+            .attr("fill", "#888");
 
-barChart.selectAll("text")
-        .data(data).enter()
-        .append("svg:text")
-        .attr("x", (_, index) => index * (barWidth + barSpacing))
-        .attr("y", datum => graphHeight - datum.value * 100)
-        .text(datum => datum.tag)
-        .attr("dx", barWidth/2)
-        .attr("dy", "2em")
-        .attr("fill", "white");
+    barChart.selectAll("text")
+            .data(data).enter()
+            .append("svg:text")
+            .attr("x", (_, index) => index * (barWidth + barSpacing))
+            .attr("y", datum => graphHeight - datum.value * 100)
+            .text(datum => datum.tag)
+            .attr("dx", barWidth/2)
+            .attr("dy", "2em")
+            .attr("fill", "white");
 
-document.querySelector(".guess").onchange = event => {
-    data[1].tag = +event.target.value;
-    const text = barChart.selectAll("text").data(data);
-    text.merge(text).text(d => d.tag);
+    document.querySelector(".guess").onchange = event => {
+        data[1].tag = +event.target.value;
+        const text = barChart.selectAll("text").data(data);
+        text.merge(text).text(d => d.tag);
+    };
+
+    document.querySelector(".confirm").onclick = () => {
+        document.querySelector(".confirm").style.display = "none";
+        const infoDom = document.querySelector(".info");
+        const guess = Number(document.querySelector(".guess").value);
+        const truth = data[1].value;
+        infoDom.innerHTML = `
+            Your guess: ${guess} <br />
+            True value: ${truth.toFixed(2)} <br />
+            You're off by ${((guess - truth) / truth * 100).toFixed(2)}%
+        `;
+        document.querySelector('.play-again').style.display = 'block';
+
+        history.push({
+            scene: uiState.scene,
+            truth,
+            guess,
+            time: Date.now(),
+        });
+        localStorage.history = JSON.stringify(history)
+    };
+}
+
+function resetUI() {
+    document.querySelector('.graph').innerHTML = '';
+    document.querySelector(".info").innerHTML = '';
+    document.querySelector('.guess').value = 0;
+    document.querySelector('.play-again').style.display = 'none';
+    document.querySelector('.confirm').style.display = 'block';
+}
+
+document.querySelector('.play-again').onclick = () => {
+    resetUI();
+    if (uiState.scene === 'bar') {
+        renderBarScene();
+    }
 };
 
-document.querySelector(".confirm").onclick = () => {
-    document.querySelector(".confirm").style.display = "none";
-    const infoDom = document.querySelector(".info");
-    const guess = Number(document.querySelector(".guess").value);
-    const truth = data[1].value;
-    infoDom.innerHTML = `
-        Your guess: ${guess} <br />
-        True value: ${truth.toFixed(2)} <br />
-        You're off by ${((guess - truth) / truth * 100).toFixed(2)}%
-    `;
-    document.querySelector('.play-again').style.display = 'block';
+renderBarScene();
 
-    history.push({
-        type: 'bar',
-        truth,
-        guess,
-        time: Date.now(),
-    });
-    localStorage.history = JSON.stringify(history)
-};
-
-document.querySelector('.play-again').onclick =
-        () => window.location.reload();
